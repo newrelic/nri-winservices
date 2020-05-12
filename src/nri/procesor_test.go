576e93c0691da4ff11698b2ada71cfbacca1ed5f
paologallinaharbur <paologallina1992@gmail.com>
2020-05-12 15:27:32 +0200

    fix (test): fixed tests

--- src/nri/procesor_test.go
+++ src/nri/procesor_test.go
@@ -1,6 +1,8 @@
 package nri
 
 import (
+	"errors"
+	"github.com/newrelic/nri-winservices/src/scraper"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,38 +11,57 @@ import (
 	dto "github.com/prometheus/client_model/go"
 )
 
-var metricFamlily = dto.MetricFamily{
-	Name: &(&struct{ x string }{"wmi_service_start_mode"}).x,
-	Type: &(&struct{ x dto.MetricType }{dto.MetricType_GAUGE}).x,
+var gauge = dto.MetricType_GAUGE
+var metricFamlilyService = dto.MetricFamily{
+	Name: strPtr("wmi_service_start_mode"),
+	Type: &gauge,
 	Metric: []*dto.Metric{
 		{
 			Label: []*dto.LabelPair{
 				{
-					Name:  &(&struct{ x string }{"name"}).x,
-					Value: &(&struct{ x string }{"rpcss"}).x,
+					Name:  strPtr("name"),
+					Value: strPtr("rpcss"),
 				},
 				{
-					Name:  &(&struct{ x string }{"start_mode"}).x,
-					Value: &(&struct{ x string }{"auto"}).x,
+					Name:  strPtr("start_mode"),
+					Value: strPtr("auto"),
 				},
 			},
 			Gauge: &dto.Gauge{
-				Value: &(&struct{ x float64 }{1}).x,
+				Value: float64Ptr(1),
 			},
 		},
 		{
 			Label: []*dto.LabelPair{
 				{
-					Name:  &(&struct{ x string }{"name"}).x,
-					Value: &(&struct{ x string }{"rpcss"}).x,
+					Name:  strPtr("name"),
+					Value: strPtr("rpcss"),
 				},
 				{
-					Name:  &(&struct{ x string }{"start_mode"}).x,
-					Value: &(&struct{ x string }{"boot"}).x,
+					Name:  strPtr("start_mode"),
+					Value: strPtr("boot"),
 				},
 			},
 			Gauge: &dto.Gauge{
-				Value: &(&struct{ x float64 }{0}).x,
+				Value: float64Ptr(0),
+			},
+		},
+	},
+}
+
+var metricFamlilyServiceHostname = dto.MetricFamily{
+	Name: strPtr("wmi_cs_hostname"),
+	Type: &gauge,
+	Metric: []*dto.Metric{
+		{
+			Label: []*dto.LabelPair{
+				{
+					Name:  strPtr("hostname"),
+					Value: strPtr("test-hostname"),
+				},
+			},
+			Gauge: &dto.Gauge{
+				Value: float64Ptr(1),
 			},
 		},
 	},
@@ -48,19 +69,44 @@ var metricFamlily = dto.MetricFamily{
 
 func TestCreateEntities(t *testing.T) {
 	i, _ := integration.New("integrationName", "integrationVersion")
-	rules := loadRules().EntityRules[0]
-	entityMap := createEntities(i, metricFamlily, rules)
+	rules := loadRules()
+	mfbn := scraper.MetricFamiliesByName{"wmi_service_start_mode": metricFamlilyService, "wmi_cs_hostname": metricFamlilyServiceHostname}
+
+	entityMap, err := createEntities(i, mfbn, rules)
+
+	assert.Nil(t, err)
 	_, ok := entityMap["rpcss"]
-	assert.True(t, ok) //todo improve the assert
+	assert.True(t, ok)
+
+	assert.Equal(t, i.Entities[0].Name(), "test-hostname:rpcss")
+
 }
+
+func TestCreateEntitiesFail(t *testing.T) {
+	i, _ := integration.New("integrationName", "integrationVersion")
+	rules := loadRules()
+	mfbn := scraper.MetricFamiliesByName{"wmi_service_start_mode": metricFamlilyService}
+	_, err := createEntities(i, mfbn, rules)
+
+	assert.Equal(t, err, errors.New("hostName Metric not found"))
+}
+
 func TestProccessMetricGauge(t *testing.T) {
 	i, _ := integration.New("integrationName", "integrationVersion")
+	rules := loadRules()
+	mfbn := scraper.MetricFamiliesByName{"wmi_service_start_mode": metricFamlilyService, "wmi_cs_hostname": metricFamlilyServiceHostname}
 
-	rules := loadRules().EntityRules[0]
-	entityMap := createEntities(i, metricFamlily, rules)
-
-	err := processMetricGauge(metricFamlily, rules, entityMap)
+	entityMap, err := createEntities(i, mfbn, rules)
+	assert.Nil(t, err)
+	err = processMetricGauge(metricFamlilyService, rules, entityMap)
 	assert.NoError(t, err)
-	// i.Publish()
 
+}
+
+func strPtr(s string) *string {
+	return &s
+}
+
+func float64Ptr(f float64) *float64 {
+	return &f
 }
