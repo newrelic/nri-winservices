a010f8cc3157bd204f6152687efa2f094be99fb9
Guillermo Sanchez Gavier <gsanchez@newrelic.com>
2020-05-15 19:04:58 +0200

    add displayName and pid metadata

--- src/nri/procesor_test.go
+++ src/nri/procesor_test.go
@@ -2,16 +2,50 @@ package nri
 
 import (
 	"errors"
-	"github.com/newrelic/nri-winservices/src/scraper"
 	"testing"
 
-	"github.com/stretchr/testify/require"
-
 	"github.com/newrelic/infra-integrations-sdk/integration"
+	"github.com/newrelic/nri-winservices/src/scraper"
 	dto "github.com/prometheus/client_model/go"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	serviceName        = "rpcss"
+	serviceStartMode   = "auto"
+	serviceDisplayName = "Remote Procedure Call (RPC)"
+	servicePid         = "668"
+	hostname           = "test-hostname"
 )
 
 var gauge = dto.MetricType_GAUGE
+
+var metricFamlilyServiceInfo = dto.MetricFamily{
+	Name: strPtr("wmi_service_info"),
+	Type: &gauge,
+	Metric: []*dto.Metric{
+		{
+			Label: []*dto.LabelPair{
+				{
+					Name:  strPtr("name"),
+					Value: strPtr(serviceName),
+				},
+				{
+					Name:  strPtr("display_name"),
+					Value: strPtr(serviceDisplayName),
+				},
+				{
+					Name:  strPtr("process_id"),
+					Value: strPtr(servicePid),
+				},
+			},
+			Gauge: &dto.Gauge{
+				Value: float64Ptr(1),
+			},
+		},
+	},
+}
 var metricFamlilyService = dto.MetricFamily{
 	Name: strPtr("wmi_service_start_mode"),
 	Type: &gauge,
@@ -20,11 +54,11 @@ var metricFamlilyService = dto.MetricFamily{
 			Label: []*dto.LabelPair{
 				{
 					Name:  strPtr("name"),
-					Value: strPtr("rpcss"),
+					Value: strPtr(serviceName),
 				},
 				{
 					Name:  strPtr("start_mode"),
-					Value: strPtr("auto"),
+					Value: strPtr(serviceStartMode),
 				},
 			},
 			Gauge: &dto.Gauge{
@@ -48,7 +82,6 @@ var metricFamlilyService = dto.MetricFamily{
 		},
 	},
 }
-
 var metricFamlilyServiceHostname = dto.MetricFamily{
 	Name: strPtr("wmi_cs_hostname"),
 	Type: &gauge,
@@ -57,7 +90,7 @@ var metricFamlilyServiceHostname = dto.MetricFamily{
 			Label: []*dto.LabelPair{
 				{
 					Name:  strPtr("hostname"),
-					Value: strPtr("test-hostname"),
+					Value: strPtr(hostname),
 				},
 			},
 			Gauge: &dto.Gauge{
@@ -70,32 +103,44 @@ var metricFamlilyServiceHostname = dto.MetricFamily{
 func TestCreateEntities(t *testing.T) {
 	i, _ := integration.New("integrationName", "integrationVersion")
 	rules := loadRules()
-	mfbn := scraper.MetricFamiliesByName{"wmi_service_start_mode": metricFamlilyService, "wmi_cs_hostname": metricFamlilyServiceHostname}
-	validator := NewValidator("rpcss", "", "")
+	mfbn := scraper.MetricFamiliesByName{
+		"wmi_service_info":       metricFamlilyServiceInfo,
+		"wmi_service_start_mode": metricFamlilyService,
+		"wmi_cs_hostname":        metricFamlilyServiceHostname,
+	}
+
+	validator := NewValidator(serviceName, "", "")
 	entityMap, err := createEntities(i, mfbn, rules, validator)
 	require.NoError(t, err)
-	_, ok := entityMap["rpcss"]
+	_, ok := entityMap[serviceName]
 	require.True(t, ok)
 	require.Len(t, i.Entities, 1)
-	require.Equal(t, i.Entities[0].Name(), "test-hostname:rpcss")
-
+	require.Equal(t, i.Entities[0].Name(), hostname+":"+serviceName)
 }
 
 func TestCreateEntitiesFail(t *testing.T) {
 	i, _ := integration.New("integrationName", "integrationVersion")
 	rules := loadRules()
-	mfbn := scraper.MetricFamiliesByName{"wmi_service_start_mode": metricFamlilyService}
+	mfbn := scraper.MetricFamiliesByName{
+		"wmi_service_info":       metricFamlilyServiceInfo,
+		"wmi_service_start_mode": metricFamlilyService,
+		// exclude host name metrics from family
+		// "wmi_cs_hostname":        metricFamlilyServiceHostname,
+	}
 
-	validator := NewValidator("rpcss", "", "")
+	validator := NewValidator(serviceName, "", "")
 	_, err := createEntities(i, mfbn, rules, validator)
-
 	require.Equal(t, err, errors.New("hostname Metric not found"))
 }
 
 func TestNoServiceNameAllowed(t *testing.T) {
 	i, _ := integration.New("integrationName", "integrationVersion")
 	rules := loadRules()
-	mfbn := scraper.MetricFamiliesByName{"wmi_service_start_mode": metricFamlilyService, "wmi_cs_hostname": metricFamlilyServiceHostname}
+	mfbn := scraper.MetricFamiliesByName{
+		"wmi_service_info":       metricFamlilyServiceInfo,
+		"wmi_service_start_mode": metricFamlilyService,
+		"wmi_cs_hostname":        metricFamlilyServiceHostname,
+	}
 
 	validator := NewValidator("", "", "")
 	entityMap, err := createEntities(i, mfbn, rules, validator)
@@ -110,14 +155,25 @@ func TestNoServiceNameAllowed(t *testing.T) {
 func TestProccessMetricGauge(t *testing.T) {
 	i, _ := integration.New("integrationName", "integrationVersion")
 	rules := loadRules()
-	mfbn := scraper.MetricFamiliesByName{"wmi_service_start_mode": metricFamlilyService, "wmi_cs_hostname": metricFamlilyServiceHostname}
+	mfbn := scraper.MetricFamiliesByName{
+		"wmi_service_info":       metricFamlilyServiceInfo,
+		"wmi_service_start_mode": metricFamlilyService,
+		"wmi_cs_hostname":        metricFamlilyServiceHostname,
+	}
 
-	validator := NewValidator("rpcss", "", "")
+	validator := NewValidator(serviceName, "", "")
 	entityMap, err := createEntities(i, mfbn, rules, validator)
 	require.NoError(t, err)
-	err = processMetricGauge(metricFamlilyService, rules, entityMap)
+	// process info metrics
+	err = processMetricGauge(metricFamlilyServiceInfo, rules, entityMap)
 	require.NoError(t, err)
-
+	metadata := entityMap[serviceName].GetMetadata()
+	assert.Equal(t, serviceDisplayName, metadata["windowsService.displayName"])
+	assert.Equal(t, servicePid, metadata["windowsService.processId"])
+	// process startmode metrics
+	err = processMetricGauge(metricFamlilyService, rules, entityMap)
+	assert.NoError(t, err)
+	assert.Equal(t, serviceStartMode, metadata["windowsService.startMode"])
 }
 
 func strPtr(s string) *string {
--- src/nri/rules.go
+++ src/nri/rules.go
@@ -4,29 +4,47 @@ import (
 	"fmt"
 )
 
+// EntityRules represents rules to convert prometheus metrics into NewRelic entities.
 type EntityRules struct {
 	EntityType string        `yaml:"type"`
 	EntityName EntityName    `yaml:"name"`
 	Metrics    []MetricRules `yaml:"metrics"`
 }
+
+// EntityName indicates which metrics labels use to form the unique entity name and displayName
+// for windows services the entity name is hostname:serviceName.
 type EntityName struct {
 	Metric                string `yaml:"from_metric"`
-	MetricLabel           string `yaml:"use_label"`
+	Label                 string `yaml:"name_label"`
+	DisplayNameLabel      string `yaml:"display_name_label"`
 	HostnameMetric        string `yaml:"hostname_metric"`
-	HostnameMetricLabel   string `yaml:"hostname_label"`
+	HostnameLabel         string `yaml:"hostname_label"`
 	HostnameNrdbLabelName string `yaml:"hostname_nrdb_name"`
 }
+
+// MetricRules describe the metrics that compose the entity.
+//
+// prometheus enums metrics are generally send using following style:
+// 	wmi_service_start_mode{name="wersvc",start_mode="auto"} 0
+// 	wmi_service_start_mode{name="wersvc",start_mode="disabled"} 0
+// 	wmi_service_start_mode{name="wersvc",start_mode="manual"} 1
+// using EnumMetric=true will only send the metric with value 1 with the corresponding attribute
+//
+// for promethus *_info metrics no metric will be send, just metadata.
 type MetricRules struct {
 	ProviderName string      `yaml:"provider_name"`
 	MetricType   string      `yaml:"type"`
 	NrdbName     string      `yaml:"nrdb_name"`
-	SkipValue    float64     `yaml:"skip_value"`
+	EnumMetric   bool        `yaml:"enum_metric"`
+	InfoMetric   bool        `yaml:"info_metric"`
 	Attributes   []Attribute `yaml:"attributes"`
 }
+
+// Attribute describe metrics attributes to be add.
 type Attribute struct {
 	Label            string `yaml:"provider_name"`
 	NrdbLabelName    string `yaml:"nrdb_name"`
-	IsEntityMetadata bool   `yaml:"entity_attribute"`
+	IsEntityMetadata bool   `yaml:"entity_metadata"` // when true this attribute will be use as metadata.
 }
 
 func loadRules() EntityRules {
@@ -35,27 +53,44 @@ func loadRules() EntityRules {
 
 		EntityType: "WindowsService",
 		EntityName: EntityName{
-			Metric:                "wmi_service_start_mode",
-			MetricLabel:           "name",
+			Metric:                "wmi_service_info",
+			Label:                 "name",
+			DisplayNameLabel:      "display_name",
 			HostnameMetric:        "wmi_cs_hostname",
-			HostnameMetricLabel:   "hostname",
+			HostnameLabel:         "hostname",
 			HostnameNrdbLabelName: "windowsService.hostname",
 		},
 		Metrics: []MetricRules{
 			{
+				ProviderName: "wmi_service_info",
+				InfoMetric:   true,
+				Attributes: []Attribute{
+					{
+						Label:            "name",
+						NrdbLabelName:    "windowsService.name",
+						IsEntityMetadata: true,
+					},
+					{
+						Label:            "display_name",
+						NrdbLabelName:    "windowsService.displayName",
+						IsEntityMetadata: true,
+					},
+					{
+						Label:            "process_id",
+						NrdbLabelName:    "windowsService.processId",
+						IsEntityMetadata: true,
+					},
+				},
+			},
+			{
 				ProviderName: "wmi_service_start_mode",
 				MetricType:   "gauge",
 				NrdbName:     "windowsService.service.startMode",
-				SkipValue:    0,
+				EnumMetric:   true,
 				Attributes: []Attribute{
 					{
 						Label:            "start_mode",
 						NrdbLabelName:    "windowsService.startMode",
-						IsEntityMetadata: true,
-					},
-					{
-						Label:            "name",
-						NrdbLabelName:    "windowsService.name",
 						IsEntityMetadata: true,
 					},
 				},
@@ -64,16 +99,11 @@ func loadRules() EntityRules {
 				ProviderName: "wmi_service_state",
 				MetricType:   "gauge",
 				NrdbName:     "windowsService.service.state",
-				SkipValue:    0,
+				EnumMetric:   true,
 				Attributes: []Attribute{
 					{
 						Label:         "state",
 						NrdbLabelName: "windowsService.state",
-					},
-					{
-						Label:            "name",
-						NrdbLabelName:    "windowsService.name",
-						IsEntityMetadata: true,
 					},
 				},
 			},
@@ -81,16 +111,11 @@ func loadRules() EntityRules {
 				ProviderName: "wmi_service_status",
 				MetricType:   "gauge",
 				NrdbName:     "windowsService.service.status",
-				SkipValue:    0,
+				EnumMetric:   true,
 				Attributes: []Attribute{
 					{
 						Label:         "status",
 						NrdbLabelName: "windowsService.status",
-					},
-					{
-						Label:            "name",
-						NrdbLabelName:    "windowsService.name",
-						IsEntityMetadata: true,
 					},
 				},
 			},
