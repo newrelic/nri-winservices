6a54e7e265ebb12f0c6f57820a1ebc0542dbc18a
nravada <nravada@newrelic.com>
2026-02-26 21:15:35 +0530

    Upgrade window_exporter for nri-winservices (NR-525961) (#229)
    * Upgrade window_exporter for nri-winservices (NR-525961)
    
    * Upgrade nri-winservices with very latest version of windows exporter
    
    * Delete go.sum
    
    deleted go.sum as it is auto generated file.
    
    * Revert "Delete go.sum"
    
    This reverts commit 5befe1908603777de8c5b418d6efdbe7cb3cd97a.
    
    * Updated only windows_exporter version 0.31.3

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/newrelic/nri-winservices
 
-go 1.24.13
+go 1.25.6
 
 require (
 	github.com/newrelic/infra-integrations-sdk/v4 v4.2.1
--- win_build.ps1
+++ win_build.ps1
@@ -20,8 +20,8 @@ $commitHash = (git rev-parse HEAD)
 
 $exporterRepo = "github.com/prometheus-community/windows_exporter"
 $exporterBinaryName = "windows_exporter.exe"
-# Commit used by v0.30.6 of windows_exporter
-$exporterVersion = "db60c78f32185083354f16fb9d534a021f0d85f9"
+# Commit used by v0.31.3 of windows_exporter
+$exporterVersion = "fcf21bb6008679edab438661a0e882c0c7f27386"
 
 
 $env:GOPATH = go env GOPATH
